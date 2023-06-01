0d9d5d6abbd083075b125fde97cdd5365090128f
AyoubGuissouss <a.guissouss@hotmail.fr>
2023-06-01 15:22:17 -0400

    Ajout de la methode chercherNote et modifierNote

--- src/main/java/com/example/dao/GestionNoteImpl.java
+++ src/main/java/com/example/dao/GestionNoteImpl.java
@@ -1,7 +1,12 @@
 package com.example.dao;
 
+import com.example.entity.Film;
+import com.example.entity.Membre;
 import com.example.entity.Note;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Query;
+
+import java.util.List;
 
 public class GestionNoteImpl implements IGestionNote {
 
@@ -24,5 +29,37 @@ public class GestionNoteImpl implements IGestionNote {
             transaction.rollback();
         }
         return false;
+    }
+
+    @Override
+    public Note chercherNote(Film film, Membre membre) {
+        try {
+            Query query = dataManager.manager.createQuery("SELECT N FROM Note N WHERE N.film = :film and N.membre = :membre");
+            query.setParameter("film",film);
+            query.setParameter("membre",membre);
+            Note note = (Note) query.getSingleResult();
+            return note;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public boolean modifierNote(int idNote, int note) {
+        EntityTransaction transaction = dataManager.manager.getTransaction();
+        transaction.begin();
+
+        try{
+            Note noteExistante = dataManager.manager.find(Note.class,idNote);
+            noteExistante.setNote(note);
+            transaction.commit();
+            return true;
+        }catch(Exception e){
+            e.printStackTrace();
+            transaction.rollback();
+            return false;
+        }
+
     }
 }
--- src/main/java/com/example/dao/IGestionNote.java
+++ src/main/java/com/example/dao/IGestionNote.java
@@ -1,10 +1,14 @@
 package com.example.dao;
 
+import com.example.entity.Film;
+import com.example.entity.Membre;
 import com.example.entity.Note;
 
 public interface IGestionNote {
 
  boolean ajouterNote (Note note);
 
+ Note chercherNote(Film film, Membre membre);
 
+ boolean modifierNote(int idNote, int note);
 }
